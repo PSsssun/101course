6bc5d46d4687b18319c766dd517e443284a8c1cc
Peng Sun <sunpengde666@gmail.com>
2024-07-12 09:56:47 +0800

    usages of setter & getter

--- slides/getset/vector.cpp
+++ slides/getset/vector.cpp
@@ -2,10 +2,15 @@
 #include <cstddef>
 
 struct Vector {
+
+// 私有变量 m_data 和 m_size 用户不应该直接对其进行操作
 private:
     int *m_data;
     size_t m_size;
 
+// 公有函数 setSize、data 和 size 用于对私有变量进行操作
+// 私有变量的状态由公有函数进行控制，从而保证了私有变量的状态是合法的
+// 在函数内可能短暂非法，但是函数结束后，私有变量的状态必须是合法的，自洽的
 public:
     Vector() : m_data(new int[4]), m_size(4) {}
 
@@ -18,9 +23,11 @@ public:
     int *data() const {
         return m_data;
     }
-
-    size_t size() const {
-        return m_size;
+    // 用户不应该直接对 m_size 进行操作，因此不提供修改 m_size 的函数
+    // 返回值是一个右值
+    // 无法赋值 
+    size_t size() const { 
+        return m_size; 
     }
 };
 
